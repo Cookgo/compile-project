801534b9d3a54dac1c387ec85fe3e137b9c7aeb1
dkl <abazaic@163.com>
2019-06-22 18:57:03 +0800

    add_select

--- parser.py
+++ parser.py
@@ -5,6 +5,7 @@ non_ter_set = set()
 pro_set = []
 first_set = {}
 follow_set={}
+select_set={}
 fir_num = {}
 fol_num={}
 
@@ -23,8 +24,11 @@ S = 'E'
 for f in non_ter_set:
     first_set[f] = set()
     follow_set[f]=set()
+for p in pro_set:
+    i=pro_set.index(p)
+    select_set[i]=set()
 
-
+# 求first集
 def has_empty(t):
     for p in pro_set:
         if p[0] == t and p[2] == 'empty':
@@ -76,6 +80,7 @@ def get_first():
         print(first_set)
         return
 
+# 求follow集
 def loop_fo():
     for x in non_ter_set:
         if x ==S:
@@ -98,8 +103,7 @@ def loop_fo():
                                 if n==right[-1]:
                                     follow_set[x]=follow_set[x]|(follow_set[left]-{'empty'})
                                 else:continue
-                            else:break
-                        
+                            else:break                       
 def fol_change():
     for f in follow_set:
         if fol_num[f]!=len(follow_set[f]):
@@ -115,7 +119,40 @@ def get_follow():
         print(follow_set)
         return
 
+# 求select集
+def get_str_fir(str):
+    temp=set()
+    for x in str:
+        if x in ter_set:
+            temp.add(x)
+            return temp
+        elif x in non_ter_set:
+            if 'empty' in first_set[x]:
+                temp=temp|(first_set[x]-{'empty'})
+                if x==str[-1]:
+                    temp.add('empty')
+                    return temp
+            else:
+                temp=temp|first_set[x]
+                return temp
+        else:print('求串first集时错误：既不是终结符也不是非终结符')
+def get_select():
+    for p in pro_set:
+        i=pro_set.index(p)
+        left=p[0]
+        right=p[2:]
+        if right[0]=='empty':
+            select_set[i]=follow_set[left]
+        else:
+            fir=get_str_fir(right)
+            if 'empty' in fir:
+                select_set[i]=select_set[i]|fir|follow_set[left]-{'empty'}
+            else:
+                select_set[i]=select_set[i]|fir
+    print(select_set)
+
 
 if __name__ == "__main__":
     get_first()
     get_follow()
+    get_select()
